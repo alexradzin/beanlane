0b97c44fa343ff1d3a832fd8dd0d450c7ec86080
alexander <alexander.radzin@feedvisor.com>
2019-01-14 20:28:43 +0200

    added BeanLaneSpec - interface with default implementation

---
+++ src/main/java/org/beanlane/BeanLaneSpec.java
@@ -0,0 +1,35 @@
+package org.beanlane;
+
+import org.beanlane.NameExtractor.BeanNameExtractor;
+
+import java.util.function.Supplier;
+
+public interface BeanLaneSpec {
+    BeanLane bean = new BeanLane(new BeanNameExtractor());
+    BeanLane snake = new BeanLane(new NameExtractor.SnakeNameExtractor());
+    BeanLane upperSnake = new BeanLane(new NameExtractor.SnakeNameExtractor(true));
+
+    default <T> T $(Class<T> clazz) {
+        return bean.of(clazz);
+    }
+
+    default <T> String $(Supplier<T> f)  {
+        return bean.name(f);
+    }
+
+    default <T> T __(Class<T> clazz) {
+        return snake.of(clazz);
+    }
+
+    default <T> String __(Supplier<T> f)  {
+        return snake.name(f);
+    }
+
+    default <T> T ___(Class<T> clazz) {
+        return upperSnake.of(clazz);
+    }
+
+    default <T> String ___(Supplier<T> f)  {
+        return upperSnake.name(f);
+    }
+}
---
+++ src/test/java/org/beanlane/BeanLaneSpecTest.java
@@ -0,0 +1,47 @@
+package org.beanlane;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BeanLaneSpecTest implements BeanLaneSpec {
+    @Test
+    void beanPropertyName() {
+        Person p = $(Person.class);
+        assertEquals("firstName", $(p::getFirstName));
+        assertEquals("lastName", $(p::getLastName));
+        assertEquals("home", $(p::getHome));
+        assertEquals("home.zip", $(() -> p.getHome().getZip()));
+        assertEquals("home.city", $(() -> p.getHome().getCity()));
+        assertEquals("home.country", $(() -> p.getHome().getCountry()));
+        assertEquals("home.street", $(() -> p.getHome().getStreet()));
+        assertEquals("home.streetNumber", $(() -> p.getHome().getStreetNumber()));
+    }
+
+
+    @Test
+    void snakePropertyName() {
+        Person p = __(Person.class);
+        assertEquals("first_name", __(p::getFirstName));
+        assertEquals("last_name", __(p::getLastName));
+        assertEquals("home", __(p::getHome));
+        assertEquals("home.zip", __(() -> p.getHome().getZip()));
+        assertEquals("home.city", __(() -> p.getHome().getCity()));
+        assertEquals("home.country", __(() -> p.getHome().getCountry()));
+        assertEquals("home.street", __(() -> p.getHome().getStreet()));
+        assertEquals("home.street_number", __(() -> p.getHome().getStreetNumber()));
+    }
+
+    @Test
+    void upperCaseSnakePropertyName() {
+        Person p = ___(Person.class);
+        assertEquals("FIRST_NAME", ___(p::getFirstName));
+        assertEquals("LAST_NAME", ___(p::getLastName));
+        assertEquals("HOME", ___(p::getHome));
+        assertEquals("HOME.ZIP", ___(() -> p.getHome().getZip()));
+        assertEquals("HOME.CITY", ___(() -> p.getHome().getCity()));
+        assertEquals("HOME.COUNTRY", ___(() -> p.getHome().getCountry()));
+        assertEquals("HOME.STREET", ___(() -> p.getHome().getStreet()));
+        assertEquals("HOME.STREET_NUMBER", ___(() -> p.getHome().getStreetNumber()));
+    }
+}
